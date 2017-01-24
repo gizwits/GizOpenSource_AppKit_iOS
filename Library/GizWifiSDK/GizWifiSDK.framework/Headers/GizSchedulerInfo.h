d574e14514ccac406c3e1927c3aedd939c15aabf
Tom <htge@xtremeprog.com>
2017-01-24 18:29:35 +0800

    更新了sdk

--- Library/GizWifiSDK/GizWifiSDK.framework/Headers/GizSchedulerInfo.h
+++ Library/GizWifiSDK/GizWifiSDK.framework/Headers/GizSchedulerInfo.h
@@ -15,19 +15,18 @@
 
 @property (strong, nonatomic, readonly) NSString *sid;
 @property (strong, nonatomic, readonly) NSString *createdDateTime;
+/*
+ 定时任务的执行状态（预留，暂不实现）
+ */
+@property (assign, nonatomic, readonly) BOOL status;
 
 @property (strong, nonatomic) NSString *date;
 @property (strong, nonatomic) NSString *time;
 @property (strong, nonatomic) NSString *remark;
+@property (strong, nonatomic) NSArray *taskList;
+@property (assign, nonatomic) GizScheduleRepeatRule repeatRule;
 @property (strong, nonatomic) NSArray *weekdays;
 @property (strong, nonatomic) NSArray *monthDays;
-@property (strong, nonatomic) NSString *startDate;
-@property (strong, nonatomic) NSString *endDate;
-@property (assign, nonatomic) BOOL enabled;
-@property (strong, nonatomic) NSDictionary *attrs;
-
-@property (strong, nonatomic) NSArray *taskList DEPRECATED_ATTRIBUTE;
-@property (assign, nonatomic) GizScheduleRepeatRule repeatRule DEPRECATED_ATTRIBUTE;
-@property (assign, nonatomic) NSInteger repeatCount DEPRECATED_ATTRIBUTE;
+@property (assign, nonatomic) NSInteger repeatCount;
 
 @end
